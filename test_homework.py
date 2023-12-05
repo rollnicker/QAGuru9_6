6db26ab7210e7e9626b0588b6ac748a4d7083c8f
Rolnik Boris <rollnicker@mail.ru>
2023-12-06 01:11:17 +0300

    Добавил вывод на экран названия функций и их аргументы

--- test_homework.py
+++ test_homework.py
@@ -1,5 +1,5 @@
 from datetime import time
-
+import pytest
 
 def test_dark_theme_by_time():
     """
@@ -99,14 +99,17 @@ def test_readable_function():
 
 def open_browser(browser_name):
     actual_result = func_name(open_browser, browser_name)
+    print(actual_result)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
     actual_result = func_name(go_to_companyname_homepage, page_url)
+    print(actual_result)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
     actual_result = func_name(find_registration_button_on_login_page,page_url, button_text)
+    print(actual_result)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
